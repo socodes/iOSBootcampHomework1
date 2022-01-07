d070043401f35c1d303d9ed8440f4d30df11a6fb
Muhammed Didin <mdidin80@gmail.com>
2022-01-08 01:00:19 +0300

    last

--- CatchTheKenny/CatchTheKenny/ViewController.swift
+++ CatchTheKenny/CatchTheKenny/ViewController.swift
@@ -15,11 +15,12 @@ class ViewController: UIViewController {
     var kennyArray = [UIImageView]()
     var hideTimer = Timer()
     var highscore = 0
+    var imageArr = [[UIImage]]()
     
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var highscoreLabel: UILabel!
-    
+    /*
     @IBOutlet weak var kenny1: UIImageView!
     @IBOutlet weak var kenny2: UIImageView!
     @IBOutlet weak var kenny3: UIImageView!
@@ -29,7 +30,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var kenny7: UIImageView!
     @IBOutlet weak var kenny8: UIImageView!
     @IBOutlet weak var kenny9: UIImageView!
-    
+    */
+    @IBOutlet weak var kennyb: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +48,7 @@ class ViewController: UIViewController {
             highscore = newScore
             highscoreLabel.text = "Highscore: \(highscore)"
         }
-        
+        /*
         kenny1.isUserInteractionEnabled = true
         kenny2.isUserInteractionEnabled = true
         kenny3.isUserInteractionEnabled = true
@@ -56,8 +58,11 @@ class ViewController: UIViewController {
         kenny7.isUserInteractionEnabled = true
         kenny8.isUserInteractionEnabled = true
         kenny9.isUserInteractionEnabled = true
+        */
+        kennyb.isUserInteractionEnabled = true
         
-        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
+        /*
         let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
         let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
         let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
@@ -67,7 +72,9 @@ class ViewController: UIViewController {
         let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
         let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
         let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
-        
+        */
+        kennyb.addGestureRecognizer(recognizer)
+        /*
         kenny1.addGestureRecognizer(recognizer1)
         kenny2.addGestureRecognizer(recognizer2)
         kenny3.addGestureRecognizer(recognizer3)
@@ -79,22 +86,25 @@ class ViewController: UIViewController {
         kenny9.addGestureRecognizer(recognizer9)
         
         kennyArray = [kenny1,kenny2,kenny3,kenny4,kenny5,kenny6,kenny7,kenny8,kenny9]
-        
+        */
         counter = 30
         timeLabel.text = String(counter)
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
         hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideKenny), userInfo: nil, repeats: true)
         hideKenny()
+
     }
+
     
     @objc func hideKenny() {
-        for kenny in kennyArray {
-            kenny.isHidden = true
-        }
+        //for kenny in kennyArray {
+            kennyb.isHidden = false
+        //}
         
-        let random = Int(arc4random_uniform(UInt32(kennyArray.count - 1)))
+        //let random = Int(arc4random_uniform(UInt32(kennyArray.count - 1)))
         
-        kennyArray[random].isHidden = false
+        //kennyArray[random].isHidden = false
     }
     
     @objc func increaseScore() {
@@ -131,6 +141,7 @@ class ViewController: UIViewController {
                 self.counter = 30
                 self.timeLabel.text = String(self.counter)
                 self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.countDown), userInfo: nil, repeats: true)
+                
                 self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.hideKenny), userInfo: nil, repeats: true)
                 
             }
